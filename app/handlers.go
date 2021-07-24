d1380fa866ebb47edd5e84f9307831867aa8ace5
phasinthongdee <phasinthongdee@gmail.com>
2021-07-24 17:47:26 +0700

    all files

---
+++ app/handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	ZipCode string `json:"zip_code" xml:"zip_code"`
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ashish", "New Delhi", "110075"},
+		{"Phasin", "Thailand", "10120"},
+	}
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/phasinthongdee/banking"
+
+func main() {
+	Start()
+}
